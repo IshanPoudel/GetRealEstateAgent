4f3afaba39644d1557a09b31f8be25b8b96a79d3
IshanPoudel <ishan.poudel@mavs.uta.edu>
2022-06-25 14:08:37 -0500

    added house_specs updated file using threads

---
+++ get_house_specs.py
@@ -0,0 +1,78 @@
+#from the house_link get house_specs.
+# PRICE
+# BED AND BATH
+# SQ FT
+# LOT SIZE
+# year built
+# car-dependent walk score
+
+import time
+import logging
+import csv
+from bs4 import BeautifulSoup
+from redfin import get_list_of_houses
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import *
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import mysql.connector
+
+import traceback
+
+chrome_options = Options()  # Instantiate an options class for the selenium webdriver
+chrome_options.add_argument("--headless")  # So that a chrome window does not pop up
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+time.sleep(10)
+
+link = "https://www.redfin.com/TX/Dallas/3414-Lindenwood-Ave-75205/home/32067482"
+
+driver.get(link)
+
+try:
+    html_text = driver.page_source
+    # convert based on 'html parser'
+    soup = BeautifulSoup(html_text, 'html.parser')
+    # print(soup)
+except:
+    print("Could not get source")
+    print(traceback.format_exc())
+    quit()
+
+home = soup.find("div", {"id": "content"})
+
+home = home.find("div", {"data-react-server-container": "10"})
+home = home.find("div", {"class": "alongTheRail"})
+
+address = home.find_all("div", {"class": "dp-col-sm-12 dp-col-md-8"})
+
+for div_tags in address:
+    # find divs if present
+    check_divs = div_tags.find("section", {"class": "Section MainHouseInfoPanel"})
+    # print(check_divs)
+    if check_divs is not None:
+        address = check_divs
+        print("found section mainhouseinfopannel")
+
+
+home = address.find("div" , {"class" : "sectionContainer"})
+# print(home)
+home = home.find("div" , {"class" : "house-info-container"})
+home = home.find("div" , {"class" : "content clear-fix"})
+key_details = home.find_all("div" , {"class" : "keyDetailsList"})
+
+# for all divs in key_detail_list search for status , property_type , style , community , lotsize
+for details in key_details:
+    key_detail_list = details.find_all("div" )
+    for each_value in key_detail_list:
+        print(each_value.get_text())
+        print("\n")
+        #year built , lot size and community
+        #price
+        #price,sq ft
+
--- real_estate_table_for_each_value.py
+++ real_estate_table_for_each_value.py
@@ -2,9 +2,13 @@ import re
 import time
 import logging
 import csv
+import traceback
+import concurrent.futures
 from bs4 import BeautifulSoup
 from redfin import get_list_of_houses
 from redfin_individual import get_real_estate_info
+import threading
+from threading import Lock
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -19,10 +23,70 @@ import mysql.connector
 from random import randint
 
 
+
+
 db = mysql.connector.connect(host="localhost" ,
     user = "root" ,
     passwd="rootroot"  ,
     database = "real_estate")
+
+def chunks(list_to_split , n_parts):
+
+
+    n_parts = max(1, n_parts)
+    n_parts = min(n_parts , len(list_to_split))
+
+    print("Splitting into" + str(n_parts) + " parts")
+
+    chunk_size, remainder = divmod(len(list_to_split), n_parts)
+
+
+
+
+    return (list_to_split[i*chunk_size+min(i,remainder):(i+1)*chunk_size+min(i+1 , remainder)]  for i in range(n_parts))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def update_to_db(link_and_id ):
+    for link , id in link_and_id:
+        time.sleep(randint(5 , 20))
+
+        to_commit = get_real_estate_info(link)
+        query = "INSERT INTO house_agent(house_id , street_address , state_address , agent_name , AGENCY , trec ) VALUES (%s , %s , %s , %s , %s , %s)"
+        if to_commit[0]=='Error':
+            print("Error")
+        else:
+            #change value of house_agent_table
+            try:
+
+                mycursor.execute(query , (int(id) , str(to_commit[0]), str(to_commit[1]) , str(to_commit[2]) , str(to_commit[4]) , str(to_commit[3]) ) )
+                db.commit()
+
+                #change value of data_present of house_link table to true
+                Q = "UPDATE house_link SET data_present = 'true' WHERE houseID="+str(id)
+                mycursor.execute(Q)
+                db.commit()
+
+                print("Updated house with address"+ to_commit[0]+to_commit[1] + " and agent" + to_commit[2])
+
+            except Exception:
+                print("Could not update")
+                print(traceback.format_exc())
+
+
+
 
 #create secondary table for each id with the house_id from the primary table
 
@@ -35,33 +99,38 @@ db = mysql.connector.connect(host="localhost" ,
 #                                                                                                                               trec VARCHAR(10));
 
 #get value from house_link table with data_present true.
+
+
+
 mycursor = db.cursor()
 
 mycursor.execute("SELECT link , houseID FROM house_link WHERE data_present = 'false'")
 link_and_id = mycursor.fetchall()
 
-for (link , id) in link_and_id:
-    print(link)
-    print(id)
+
+split_link_into_n_parts = 5
+list_for_threads = chunks(link_and_id ,split_link_into_n_parts)
 
 
 
+#split the list into equal sizes and feed it to the function below .
+#link_and_id have to be split into equal parts.
 
-for link , id in link_and_id:
-    time.sleep(randint(5 , 20))
 
-    to_commit = get_real_estate_info(link)
-    query = "INSERT INTO house_agent(house_id , street_address , state_address , agent_name , AGENCY , trec ) VALUES (%s , %s , %s , %s , %s , %s)"
-    if to_commit[0]=='Error':
-        print("Error")
-    else:
-        #change value of house_agent_table
-        mycursor.execute(query , (int(id) , str(to_commit[0]), str(to_commit[1]) , str(to_commit[2]) , str(to_commit[4]) , str(to_commit[3]) ) )
-        db.commit()
-        #change value of data_present of house_link table to true
-        Q = "UPDATE house_link SET data_present = 'true' WHERE houseID="+str(id)
-        mycursor.execute(Q)
-        db.commit()
-        print("Updated house with address"+ to_commit[0]+to_commit[1] + " and agent" + to_commit[2])
+lock = Lock()
 
+threads = []
+
+# for house_link_and_id in list_for_threads:
+#     print(house_link_and_id)
+#     print("haha")
+
+for house_link_and_id in list_for_threads:
+    t = threading.Thread(target= update_to_db , args = (house_link_and_id , ))
+    t.start()
+    print("I started a thread")
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
 
